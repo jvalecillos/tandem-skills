b22d9ce3c71a7daf6e46b6f2441a9d8027aa67eb
Sergey Khoroshavin <skhoroshavin@gmail.com>
2026-09-23 20:42:31 +0200

    pi: inject prompt as system prompt section, require pi 0.87

--- packages/pi-tandem/extensions/tandem/pi-tandem.ts
+++ packages/pi-tandem/extensions/tandem/pi-tandem.ts
@@ -9,14 +9,7 @@ const prompt = filterCliTools(
 );
 
 export default function (pi: ExtensionAPI) {
-  pi.on("before_agent_start", async (event) => {
-    const marker = "\n\n<project_context>\n\n";
-    const idx = event.systemPrompt.indexOf(marker);
-    return {
-      systemPrompt:
-        idx === -1
-          ? `${event.systemPrompt}\n\n${prompt}`
-          : event.systemPrompt.slice(0, idx) + `\n\n${prompt}` + event.systemPrompt.slice(idx),
-    };
+  pi.on("before_agent_start", (event) => {
+    event.systemPromptOptions.sections.tandem = prompt;
   });
 }
